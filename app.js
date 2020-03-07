8a4f180d8902586bf6492f024c02b443b512623d
DevShaded <devshaded@gmail.com>
2020-03-08 00:10:55 +0100

    edit: event handling and methods

--- app.js
+++ app.js
@@ -6,6 +6,13 @@ new Vue({
             eventTitle: 'Summer Festival!',
             eventDescription: "It's back! This years summer festival will be in the beautiful countryside featuring our best line up ever!"
         },
-        newNameText: ''
-    }
+        newNameText: '',
+        guestName: []
+    },
+    methods: {
+        formSubmitted: function() {
+            this.guestName.push(this.newNameText)
+            this.newNameText = ''
+        } 
+    },
 });
