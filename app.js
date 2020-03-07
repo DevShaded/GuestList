bb1cbc66e55205d013a9a512f34cad4212f1fc13
DevShaded <devshaded@gmail.com>
2020-03-07 23:15:11 +0100

    add: vuejs code

---
+++ app.js
@@ -0,0 +1,10 @@
+new Vue({
+    el: '#app',
+    data: {
+        event: {
+            eventDate: 'August 14th - 16th',
+            eventTitle: 'Summer Festival!',
+            eventDescription: "It's back! This years summer festival will be in the beautiful countryside featuring our best line up ever!"
+        }
+    }
+});
