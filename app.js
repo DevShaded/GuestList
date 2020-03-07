decd1d0177c1b8e0f4641c6ee6c9b1e2c7591970
DevShaded <devshaded@gmail.com>
2020-03-07 23:57:24 +0100

    add: two way data binding with v-model

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ new Vue({
             eventDate: 'August 14th - 16th',
             eventTitle: 'Summer Festival!',
             eventDescription: "It's back! This years summer festival will be in the beautiful countryside featuring our best line up ever!"
-        }
+        },
+        newNameText: ''
     }
 });
